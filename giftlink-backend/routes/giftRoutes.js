029c99e406339636616400d94be59aa2bc0d8216
SV-Hello <sahilvartak8@gmail.com>
2024-12-19 23:51:12 -0500

    Working on making gifts appear

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,8 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+
 router.get('/', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
         const db = await connectToDatabase();
-        
 
         // Task 2: use the collection() method to retrieve the gift collection
         const collection = db.collection("gifts");
